a05fc66c9888e0928a56e3be17509817a9f655cd
Jokir <doner24072004@gmail.com>
2024-05-19 23:32:36 +0300

    Directives Deep Dive

--- cmp-databinding-start/src/app/app.module.ts
+++ cmp-databinding-start/src/app/app.module.ts
@@ -2,21 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+
 import { AppComponent } from './app.component';
-import { ServerElementComponent } from './server-element/server-element.component';
-import { CockpitComponent } from './cockpit/cockpit.component';
-import { GameControlComponent } from './game-control/game-control.component';
-import { OddComponent } from './odd/odd.component';
-import { EvenComponent } from './even/even.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerElementComponent,
-    CockpitComponent,
-    GameControlComponent,
-    OddComponent,
-    EvenComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
