2c269e1ef23c523884efa17933bb2685c6aaa56b
Jokir <doner24072004@gmail.com>
2024-07-18 20:33:29 +0300

    Outputting Posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { map } from 'rxjs/operators';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  loadedPosts = [];
+  loadedPosts: Post[] = [];
 
   constructor(private http: HttpClient) {}
 
@@ -52,7 +52,8 @@ export class AppComponent implements OnInit {
         return postsArray;
     }))
     .subscribe( posts => {
-      console.log(posts);
+      // ...
+      this.loadedPosts = posts;
     });
   }
 }
