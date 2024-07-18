effe21fd2d483406211711f1e9e174a937cd6d2f
Jokir <doner24072004@gmail.com>
2024-07-18 20:00:41 +0300

    Sending a POST Request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,12 @@ export class AppComponent implements OnInit {
   onCreatePost(postData: { title: string; content: string }) {
     // Send Http request
     console.log(postData);
+    this.http.post(
+      'https://ng-complete-guide-d3567-default-rtdb.europe-west1.firebasedatabase.app/post.json',
+      postData
+    ).subscribe(responseData => {
+      console.log(responseData);
+    }); // .json is exclusive only for Firebase
   }
 
   onFetchPosts() {
