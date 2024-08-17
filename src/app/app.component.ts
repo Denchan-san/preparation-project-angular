b43b064de7d43dd56d1add3e46d6bfefdb66a9fd
Jokir <doner24072004@gmail.com>
2024-08-17 17:20:28 +0300

    Standalone Components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,70 +1,8 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Post } from './post.model';
-import { PostService } from './posts.service';
-import { Subscription } from 'rxjs';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit, OnDestroy {
-  loadedPosts: Post[] = [];
-  isFetching = false;
-  error = null;
-  private errorSub: Subscription;
-
-  constructor(private http: HttpClient,
-              private postsService: PostService) {
-
-              }
-            
-  ngOnDestroy(): void {
-    this.errorSub.unsubscribe();
-  }
-
-  ngOnInit() {
-    this.errorSub = this.postsService.error.subscribe(errorMessage => {
-      this.error = errorMessage;
-    });
-
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    }, error => {
-      this.isFetching = false;
-      this.error = error.message;
-    });
-  }
-  
-  onCreatePost(postData: Post) {
-    this.postsService.createAndStorePost(postData.title, postData.content);
-  }
-
-  onFetchPosts() {
-    // Send Http request
-    this.isFetching = true;
-    this.postsService.fetchPosts().subscribe(
-      posts => {
-      this.isFetching = false;
-      this.loadedPosts = posts;
-    }, error => {
-      this.isFetching = false;
-      this.error = error.message;
-      console.log(error);
-    });
-  }
-
-  onClearPosts() {
-    // Send Http request
-    this.postsService.deletePosts().subscribe(() => {
-      this.loadedPosts = [];
-    });
-  }
-
-  onHandleError() {
-    this.error = null;
-  }
-}
+export class AppComponent {}
